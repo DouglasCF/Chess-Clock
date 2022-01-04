d3f9ff666b290afcbb5b9ea71e1bcff970aef4bb
Douglas Fornaro <douglasfornaro@hotmail.com>
2022-01-04 15:34:30 -0300

    [CC-11] Add incremental time (#14)
    * [CC-11] Add incremental time
    
    - Refactor and created the TimeText.
    
    * - Created the IncrementalText.

--- androidApp/src/main/java/br/com/fornaro/chessclock/android/ui/home/HomeScreen.kt
+++ androidApp/src/main/java/br/com/fornaro/chessclock/android/ui/home/HomeScreen.kt
@@ -2,7 +2,6 @@ package br.com.fornaro.chessclock.android.ui.home
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -11,7 +10,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Pause
 import androidx.compose.material.icons.filled.PlayArrow
@@ -20,14 +18,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import br.com.fornaro.chessclock.Game
 import br.com.fornaro.chessclock.android.theme.Dimens
@@ -42,22 +37,24 @@ fun HomeScreen(viewModel: HomeViewModel = hiltViewModel()) {
 
     LaunchedEffect(LocalContext.current) {
         launch(Dispatchers.IO) {
-        while (true) {
-            if (game.whiteTimeRemaining > 0 && game.isWhiteMove && game.isPlaying) {
-                delay(Game.delay)
-                viewModel.decreaseWhiteTime()
+            while (true) {
+                if (game.whiteTimeRemaining > 0 && game.isWhiteMove && game.isPlaying) {
+                    delay(Game.delay)
+                    viewModel.decreaseWhiteTime()
+                }
             }
-        }}
+        }
     }
 
     LaunchedEffect(LocalContext.current) {
         launch(Dispatchers.IO) {
-        while (true) {
-            if (game.blackTimeRemaining > 0 && !game.isWhiteMove && game.isPlaying) {
-                delay(Game.delay)
-                viewModel.decreaseBlackTime()
+            while (true) {
+                if (game.blackTimeRemaining > 0 && !game.isWhiteMove && game.isPlaying) {
+                    delay(Game.delay)
+                    viewModel.decreaseBlackTime()
+                }
             }
-        }}
+        }
     }
 
     Content(
@@ -75,63 +72,57 @@ private fun Content(
     onWhitePressedClock: () -> Unit = {},
     onBlackPressedClock: () -> Unit = {},
 ) {
-
-    val modifier = Modifier
-        .fillMaxWidth()
-        .padding(Dimens.default)
-
-    Column {
-        Box(
-            modifier = Modifier
-                .background(Color.White)
-                .clickable { onWhitePressedClock() }
-                .then(if (game.isWhiteMove) modifier.weight(1f) else modifier)
-                .rotate(180f),
-            contentAlignment = Alignment.BottomEnd
-        ) {
-            Text(
+    Box {
+        Column {
+            TimeText(
+                onClickAction = onWhitePressedClock,
+                backgroundColor = Color.White,
+                modifier = (if (game.isWhiteMove) Modifier.weight(1f) else Modifier).rotate(180f),
                 text = game.whiteTimeRemainingString,
-                fontWeight = FontWeight.Black,
-                fontSize = 32.sp,
+                textColor = Color.Black,
             )
-        }
 
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(Color.Gray)
-                .padding(Dimens.default),
-            horizontalArrangement = Arrangement.SpaceAround
-        ) {
-            OutlinedButton(
-                colors = ButtonDefaults.outlinedButtonColors(backgroundColor = Color.Gray),
-                onClick = { /*TODO*/ }) {
-                Image(imageVector = Icons.Default.Settings, contentDescription = null)
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.Gray)
+                    .padding(Dimens.default),
+                horizontalArrangement = Arrangement.SpaceAround
+            ) {
+                OutlinedButton(
+                    colors = ButtonDefaults.outlinedButtonColors(backgroundColor = Color.Gray),
+                    onClick = { /*TODO*/ }) {
+                    Image(imageVector = Icons.Default.Settings, contentDescription = null)
+                }
+                OutlinedButton(
+                    colors = ButtonDefaults.outlinedButtonColors(backgroundColor = Color.Gray),
+                    onClick = { changePlayPause() }) {
+                    Image(
+                        imageVector = if (game.isPlaying) Icons.Default.Pause else Icons.Default.PlayArrow,
+                        contentDescription = null
+                    )
+                }
             }
-            OutlinedButton(
-                colors = ButtonDefaults.outlinedButtonColors(backgroundColor = Color.Gray),
-                onClick = { changePlayPause() }) {
-                Image(
-                    imageVector = if (game.isPlaying) Icons.Default.Pause else Icons.Default.PlayArrow,
-                    contentDescription = null
-                )
-            }
-        }
 
-        Box(
-            modifier = Modifier
-                .background(Color.Black)
-                .clickable { onBlackPressedClock() }
-                .then(if (!game.isWhiteMove) modifier.weight(1f) else modifier),
-            contentAlignment = Alignment.BottomStart
-        ) {
-            Text(
+            TimeText(
+                onClickAction = onBlackPressedClock,
+                backgroundColor = Color.Black,
+                modifier = if (!game.isWhiteMove) Modifier.weight(1f) else Modifier,
                 text = game.blackTimeRemainingString,
-                fontWeight = FontWeight.Black,
-                fontSize = 32.sp,
-                color = Color.White,
+                textColor = Color.White,
             )
         }
+
+        IncrementalText(
+            value = game.incrementTimeString,
+            modifier = Modifier.rotate(180f),
+            horizontalArrangement = Arrangement.End,
+            color = Color.Black
+        )
+        IncrementalText(
+            value = game.incrementTimeString,
+            color = Color.White
+        )
     }
 }
 
--- iosApp/iosApp/IncrementalText.swift
+++ iosApp/iosApp/IncrementalText.swift
@@ -1,7 +1,7 @@
 import SwiftUI
 
 struct IncrementalText: View {
-    var value: Int64
+    var value: String
     
     var body: some View {
         VStack {
@@ -9,10 +9,10 @@ struct IncrementalText: View {
             HStack(spacing: 2) {
                 Image(systemName: "arrow.up")
                     .font(.system(size: 14))
-                Text(String(value))
+                Text(value)
                     .font(.system(size: 16))
             }
-            .opacity(value == 0 ? 0.3 : 1)
+            .opacity(value == "0" ? 0.3 : 1)
             .padding()
         }
     }
@@ -20,6 +20,6 @@ struct IncrementalText: View {
 
 struct IncrementalText_Previews: PreviewProvider {
     static var previews: some View {
-        IncrementalText(value: 3)
+        IncrementalText(value: "3")
     }
 }
