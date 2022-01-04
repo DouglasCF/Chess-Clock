d863b903f6bb0365def511abf15c5b9b2eee687d
Douglas Fornaro <douglasfornaro@hotmail.com>
2022-01-04 14:50:50 -0300

    [CC-11] Add incremental time
    - Refactor and created the TimeText.

--- androidApp/src/main/java/br/com/fornaro/chessclock/android/ui/home/HomeScreen.kt
+++ androidApp/src/main/java/br/com/fornaro/chessclock/android/ui/home/HomeScreen.kt
@@ -2,16 +2,13 @@ package br.com.fornaro.chessclock.android.ui.home
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Pause
 import androidx.compose.material.icons.filled.PlayArrow
@@ -20,14 +17,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import br.com.fornaro.chessclock.Game
 import br.com.fornaro.chessclock.android.theme.Dimens
@@ -42,22 +36,24 @@ fun HomeScreen(viewModel: HomeViewModel = hiltViewModel()) {
 
     LaunchedEffect(LocalContext.current) {
         launch(Dispatchers.IO) {
-        while (true) {
-            if (game.whiteTimeRemaining > 0 && game.isWhiteMove && game.isPlaying) {
-                delay(Game.delay)
-                viewModel.decreaseWhiteTime()
+            while (true) {
+                if (game.whiteTimeRemaining > 0 && game.isWhiteMove && game.isPlaying) {
+                    delay(Game.delay)
+                    viewModel.decreaseWhiteTime()
+                }
             }
-        }}
+        }
     }
 
     LaunchedEffect(LocalContext.current) {
         launch(Dispatchers.IO) {
-        while (true) {
-            if (game.blackTimeRemaining > 0 && !game.isWhiteMove && game.isPlaying) {
-                delay(Game.delay)
-                viewModel.decreaseBlackTime()
+            while (true) {
+                if (game.blackTimeRemaining > 0 && !game.isWhiteMove && game.isPlaying) {
+                    delay(Game.delay)
+                    viewModel.decreaseBlackTime()
+                }
             }
-        }}
+        }
     }
 
     Content(
@@ -76,25 +72,14 @@ private fun Content(
     onBlackPressedClock: () -> Unit = {},
 ) {
 
-    val modifier = Modifier
-        .fillMaxWidth()
-        .padding(Dimens.default)
-
     Column {
-        Box(
-            modifier = Modifier
-                .background(Color.White)
-                .clickable { onWhitePressedClock() }
-                .then(if (game.isWhiteMove) modifier.weight(1f) else modifier)
-                .rotate(180f),
-            contentAlignment = Alignment.BottomEnd
-        ) {
-            Text(
-                text = game.whiteTimeRemainingString,
-                fontWeight = FontWeight.Black,
-                fontSize = 32.sp,
-            )
-        }
+        TimeText(
+            onClickAction = onWhitePressedClock,
+            backgroundColor = Color.White,
+            modifier = (if (game.isWhiteMove) Modifier.weight(1f) else Modifier).rotate(180f),
+            text = game.whiteTimeRemainingString,
+            textColor = Color.Black,
+        )
 
         Row(
             modifier = Modifier
@@ -118,20 +103,13 @@ private fun Content(
             }
         }
 
-        Box(
-            modifier = Modifier
-                .background(Color.Black)
-                .clickable { onBlackPressedClock() }
-                .then(if (!game.isWhiteMove) modifier.weight(1f) else modifier),
-            contentAlignment = Alignment.BottomStart
-        ) {
-            Text(
-                text = game.blackTimeRemainingString,
-                fontWeight = FontWeight.Black,
-                fontSize = 32.sp,
-                color = Color.White,
-            )
-        }
+        TimeText(
+            onClickAction = onBlackPressedClock,
+            backgroundColor = Color.Black,
+            modifier = if (!game.isWhiteMove) Modifier.weight(1f) else Modifier,
+            text = game.blackTimeRemainingString,
+            textColor = Color.White,
+        )
     }
 }
 
