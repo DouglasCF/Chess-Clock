2dd543650be6cd4c6054d787c196a1e9dce2acd5
Douglas Fornaro <douglasfornaro@hotmail.com>
2022-01-13 13:14:45 -0300

    CC-30: Improve icon and color (#31)
    * Icons and colors for iOS
    
    * Icons and colors for android

--- androidApp/src/main/java/br/com/fornaro/chessclock/android/theme/Color.kt
+++ androidApp/src/main/java/br/com/fornaro/chessclock/android/theme/Color.kt
@@ -3,10 +3,10 @@ package br.com.fornaro.chessclock.android.theme
 import androidx.compose.ui.graphics.Color
 
 val PrimaryColor = Color(0xFFFFFFFF)
-val SecondaryColor = Color(0xFF3F51B5)
+val SecondaryColor = Color(0xFF300063)
 
 val Victory = Color(0xFF009900)
 val Lose = Color(0xFFDD0000)
 
-val GameMode = Color(0xFFFF0000)
-val GameModeSelected = Color(0xFF990000)
+val GameMode = Color(0xFF8B21FC)
+val GameModeSelected = Color(0xFF300063)
