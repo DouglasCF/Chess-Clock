aa4a0dc42566a2a18de50c04013dc2ea8f3d5a58
Douglas Fornaro <douglasfornaro@hotmail.com>
2022-02-18 14:26:40 -0300

    [CC-18] Full screen option
    - Created the UI.
    - Created a ModelUi for SettingsScreen to better handle the screen.
    - Making the UI working.

--- androidApp/src/main/java/br/com/fornaro/chessclock/android/ui/settings/SettingsScreen.kt
+++ androidApp/src/main/java/br/com/fornaro/chessclock/android/ui/settings/SettingsScreen.kt
@@ -2,12 +2,14 @@ package br.com.fornaro.chessclock.android.ui.settings
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Switch
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
@@ -15,9 +17,11 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import br.com.fornaro.chessclock.android.theme.Dimens
@@ -26,14 +30,16 @@ import br.com.fornaro.chessclock.model.GameMode
 @Composable
 fun SettingsScreen(viewModel: SettingsViewModel = hiltViewModel()) {
 
-    val gameModes by viewModel.gameModes.collectAsState(emptyList())
+    val uiState by viewModel.uiState.collectAsState()
 
     Scaffold(
         topBar = { TopBar(backAction = viewModel::onBackButtonClicked) },
         content = {
             Content(
-                gameModes = gameModes,
-                gameModeClickAction = viewModel::onGameModeButtonClicked
+                gameModes = uiState.gameModes,
+                gameModeClickAction = viewModel::onGameModeButtonClicked,
+                fullScreen = uiState.fullScreen,
+                fullScreenClickAction = viewModel::onFullScreenOptionClicked,
             )
         }
     )
@@ -56,7 +62,9 @@ fun TopBar(
 @Composable
 fun Content(
     gameModes: List<GameMode>,
+    fullScreen: Boolean,
     gameModeClickAction: (Int) -> Unit,
+    fullScreenClickAction: () -> Unit,
 ) = Column {
     Text(
         text = "Game Mode",
@@ -76,4 +84,31 @@ fun Content(
             )
         }
     }
+    Row(
+        modifier = Modifier
+            .padding(horizontal = Dimens.default)
+            .padding(top = Dimens.default)
+    ) {
+        Text(
+            text = "App in full screen mode",
+            color = Color.Black,
+            modifier = Modifier
+                .align(Alignment.CenterVertically)
+                .weight(1f)
+        )
+        Switch(
+            checked = fullScreen,
+            onCheckedChange = { fullScreenClickAction() },
+
+            )
+    }
 }
+
+@Preview(showBackground = true)
+@Composable
+fun Preview() = Content(
+    gameModes = listOf(GameMode(1, 1, true), GameMode(2, 2, false)),
+    fullScreen = true,
+    gameModeClickAction = {},
+    fullScreenClickAction = {},
+)
--- androidApp/src/main/java/br/com/fornaro/chessclock/android/ui/settings/SettingsViewModel.kt
+++ androidApp/src/main/java/br/com/fornaro/chessclock/android/ui/settings/SettingsViewModel.kt
@@ -1,8 +1,13 @@
 package br.com.fornaro.chessclock.android.ui.settings
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import br.com.fornaro.chessclock.model.GameMode
 import br.com.fornaro.chessclock.repositories.GameModeRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -11,7 +16,20 @@ class SettingsViewModel @Inject constructor(
     private val gameModeRepository: GameModeRepository,
 ) : ViewModel() {
 
-    val gameModes get() = gameModeRepository.gameModes
+    private val _uiState = MutableStateFlow(UiModel())
+    val uiState get() = _uiState
+
+    init {
+        observeGameModes()
+    }
+
+    private fun observeGameModes() {
+        viewModelScope.launch {
+            gameModeRepository.gameModes.collect {
+                _uiState.value = uiState.value.copy(gameModes = it)
+            }
+        }
+    }
 
     fun onBackButtonClicked() {
         navigation.back()
@@ -20,4 +38,15 @@ class SettingsViewModel @Inject constructor(
     fun onGameModeButtonClicked(index: Int) {
         gameModeRepository.selectGameMode(index)
     }
+
+    fun onFullScreenOptionClicked() {
+        // persist option
+        _uiState.value = uiState.value.copy(fullScreen = !uiState.value.fullScreen)
+        // apply full screen option
+    }
 }
+
+data class UiModel(
+    val gameModes: List<GameMode> = emptyList(),
+    val fullScreen: Boolean = false
+)
