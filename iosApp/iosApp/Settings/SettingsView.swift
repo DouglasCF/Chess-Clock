47543eab83ce17b8121766ac71e88a6b7de36ae1
Douglas Fornaro <douglasfornaro@hotmail.com>
2022-03-15 08:59:46 -0300

    [CC-58] Custom game mode
    - [iOS] Custom game mode.

--- iosApp/iosApp/Settings/SettingsView.swift
+++ iosApp/iosApp/Settings/SettingsView.swift
@@ -3,8 +3,12 @@ import SwiftUI
 struct SettingsView: View {
     @EnvironmentObject private var settingsViewModel: SettingsViewModel
     
+    @State private var totalTime: String = ""
+    @State private var increment: String = ""
+    
     var body: some View {
         let gameModes = settingsViewModel.gameModes
+        let customGameModes = settingsViewModel.customGameModes
         
         HStack {
             VStack(alignment: .leading) {
@@ -12,21 +16,53 @@ struct SettingsView: View {
                     .fontWeight(.bold)
                     .padding([.leading, .top])
                 
-                ScrollView(.horizontal, showsIndicators: false) {
-                    LazyHStack(spacing: 12) {
-                        ForEach(gameModes.indices, id: \.self) { index in
-                            GameModeItem(gameMode: gameModes[index]) {
-                                settingsViewModel.onGameModeButtonClicked(index: Int32(index))
-                            }
-                        }
+                GameModeList(list: gameModes, action: settingsViewModel.onGameModeButtonClicked(index:))
+                GameModeList(list: customGameModes, action: settingsViewModel.onGameModeButtonClicked(index:))
+                
+                HStack {
+                    Spacer()
+                    VStack {
+                        TextField("Total time", text: $totalTime)
+                            .keyboardType(.numberPad)
+                        TextField("Increment", text: $increment)
+                            .keyboardType(.numberPad)
                     }
+                    .frame(width: 120)
+                    .textFieldStyle(.roundedBorder)
+                    Button(action: {
+                        settingsViewModel.onCreateCustomGameModeButtonClicked(totalTime: Int64(totalTime) ?? 0, increment: Int64(increment) ?? 0)
+                        totalTime = ""
+                        increment = ""
+                    }) {
+                        Text("Create a custom game")
+                            .font(.headline)
+                            .frame(height: 30)
+                            .foregroundColor(.white)
+                            .padding()
+                            .background(Color("GameMode"))
+                            .cornerRadius(8)
+                    }
+                    Spacer()
                 }
-                .frame(height: 70)
+                .padding(.horizontal)
 
                 Spacer()
             }
         }
         .navigationBarTitle("Settings")
+    }
+    
+    private func GameModeList(list: [GameModeModel], action: @escaping (Int32) -> Void) -> some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            LazyHStack(spacing: 12) {
+                ForEach(list.indices, id: \.self) { index in
+                    GameModeItem(gameMode: list[index]) {
+                        action(Int32(index))
+                    }
+                }
+            }
+        }
+        .frame(height: 70)
     }
 }
 
--- iosApp/iosApp/Settings/SettingsViewModel.swift
+++ iosApp/iosApp/Settings/SettingsViewModel.swift
@@ -4,6 +4,7 @@ import shared
 class SettingsViewModel: ObservableObject {
     
     @Published private(set) var gameModes: [GameModeModel] = []
+    @Published private(set) var customGameModes: [GameModeModel] = []
     
     private let gameModeRepository: GameModeRepository
     private let gameModeMapper: GameModeMapper
@@ -18,9 +19,27 @@ class SettingsViewModel: ObservableObject {
                 }
             }
         ) { result, error in print("compleion") }
+        self.gameModeRepository.customGameModes.collect(
+            collector: Collector<[GameMode]>() { gameModes in
+                self.customGameModes = []
+                gameModes.forEach { gameMode in
+                    self.customGameModes.append(gameModeMapper.map(gameMode: gameMode))
+                }
+            }
+        ) { result, error in print("compleion") }
     }
     
     func onGameModeButtonClicked(index: Int32) {
         gameModeRepository.selectGameMode(index: index)
+    }
+    
+    func onCustomGameModeButtonClicked(index: Int32) {
+        gameModeRepository.selectCustomGameMode(index: index)
+    }
+    
+    func onCreateCustomGameModeButtonClicked(totalTime: Int64, increment: Int64) {
+        if totalTime > 0 {
+            gameModeRepository.addCustomGame(totalTime: totalTime, increment: increment)
+        }
     }
 }
